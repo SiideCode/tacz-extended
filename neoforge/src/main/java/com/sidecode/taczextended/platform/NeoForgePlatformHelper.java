d28177b0d2ff0224c00434abd74e8b59333d132d
SiideCode <maksimka.repko@mail.ru>
2026-04-10 09:28:40 +0300

    Initial Commit

---
+++ neoforge/src/main/java/com/sidecode/taczextended/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.sidecode.taczextended.platform;
+
+import com.sidecode.taczextended.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
